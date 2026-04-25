d79680ed4603fd881f569caad918f20a27ddf20e
Hillton-sketch <collinlogan09@yahoo.com>
2026-04-25 17:43:15 -0400

    0.01

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let bow = document.querySelector('.bow-cost')
+let parsedBow = parseFloat(bow.innerHTML)
+
+let clickerCost = document.querySelector('.clicker-cost')
+let parsedClickerCost = parseFloat(clickerCost.innerHTML)
+
 
 function incrementBow() {
-    bow.innerHTML = parseFloat(bow.innerHTML) + 1
+    parsedBow += 1
+    bow.innerHTML = parsedBow
+}
+
+function buyClicker() {
+    if (parsedBow >= parsedClickerCost) {
+        parsedBow -= parsedClickerCost
+        bow.innerHTML = parsedBow
+    }
 }
