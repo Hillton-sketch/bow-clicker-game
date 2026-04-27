56da9217e6206cc344b18453fb21aee338aade7e
Hillton-sketch <collinlogan09@yahoo.com>
2026-04-26 22:22:27 -0400

    0.02

--- index.js
+++ index.js
@@ -3,16 +3,54 @@ let parsedBow = parseFloat(bow.innerHTML)
 
 let clickerCost = document.querySelector('.clicker-cost')
 let parsedClickerCost = parseFloat(clickerCost.innerHTML)
+let clickerLevel = document.querySelector('.clicker-level')
+let clickerIncrease = document.querySelector('.clicker-increase')
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
+
+let hairclipCost = document.querySelector('.hairclip-cost')
+let parsedHairclipCost = parseFloat(hairclipCost.innerHTML)
+let hairclipLevel = document.querySelector('.hairclip-level')
+let hairclipIncrease = document.querySelector('.hairclip-increase')
+let parsedHairclipIncrease = parseFloat(hairclipIncrease.innerHTML)
+
+
+
+let bpc = 1;
 
 
 function incrementBow() {
-    parsedBow += 1
-    bow.innerHTML = parsedBow
+    bow.innerHTML = Math.round(parsedBow += bpc);
 }
 
 function buyClicker() {
     if (parsedBow >= parsedClickerCost) {
-        parsedBow -= parsedClickerCost
-        bow.innerHTML = parsedBow
+        bow.innerHTML = Math.round(parsedBow -= parsedClickerCost);
+
+        clickerLevel.innerHTML = parseInt(clickerLevel.innerHTML) + 1;
+
+        parsedClickerIncrease = parseFloat((parsedClickerIncrease * 1.04).toFixed(2));
+        clickerIncrease.innerHTML = parsedClickerIncrease;
+        bpc += parsedClickerIncrease;
+
+        parsedClickerCost *=1.18;
+        clickerCost.innerHTML = Math.round(parsedClickerCost);
+
+    }
+}
+
+
+function buyHairclip() {
+    if (parsedBow >= parsedHairclipCost) {
+        bow.innerHTML = Math.round(parsedBow -= parsedHairclipCost);
+
+        hairclipLevel.innerHTML = parseInt(hairclipLevel.innerHTML) + 1;
+
+        parsedHairclipIncrease = parseFloat((parsedHairclipIncrease * 1.04).toFixed(2));
+        hairclipIncrease.innerHTML = parsedHairclipIncrease;
+        bpc += parsedHairclipIncrease;
+
+        parsedHairclipCost *=1.18;
+        hairclipCost.innerHTML = Math.round(parsedHairclipCost);
+
     }
 }
